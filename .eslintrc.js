133db65279a222d883ea46b25954874facedcc03
Steve Richards <steve.richards@gmail.com>
2018-04-01 13:41:05 -0700

    add eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  rules: {
+    'arrow-parens': 0,
+    'import/prefer-default-export': 0,
+    'no-plusplus': 0,
+    'max-len': ['error', { code: 120, ignoreTrailingComments: true }],
+  },
+};
